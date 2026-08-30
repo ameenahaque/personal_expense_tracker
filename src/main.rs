21c675c3493aab5f6b7c9c6f540faa7eb6a19c7a
Asjal Sohail Iqbal <asjalsohailiqbal@gmail.com>
2026-08-31 01:44:15 +0500

    Update expense tracker

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn read_input() -> String {
     input.trim().to_string()
 }
 
+
 fn food(food_list: &mut Vec<String>, food_rs: &mut f64) {
     
     println!(r#"
@@ -349,10 +350,7 @@ fn education(education_list: &mut Vec<String>, education_rs: &mut f64) {
 }
 
 
-fn entertainment(
-    entertainment_list: &mut Vec<String>,
-    entertainment_rs: &mut f64
-) {
+fn entertainment(entertainment_list: &mut Vec<String>,entertainment_rs: &mut f64) {
     
     println!(r#"
     ╔══════════════════════════════════════════════════════╗
@@ -423,14 +421,11 @@ fn entertainment(
 }
 
 
-fn bapc(
-    bapc_list: &mut Vec<String>,
-    bapc_rs: &mut f64
-) {
+fn bapc(bapc_list: &mut Vec<String>,bapc_rs: &mut f64) {
     
     println!(r#"
     ╔════════════════════════════════════════════════════════════╗
-    ║             BEAUTY AND PERSONAL CARE (BAPC)                ║
+    ║                  BEAUTY AND PERSONAL CARE                  ║
     ╚════════════════════════════════════════════════════════════╝
     "#);
     
@@ -450,7 +445,7 @@ fn bapc(
         match input.as_str() {
             "1" => {
                 println!(
-                    "Current Expense on Beauty and Personal Care (BAPC): Rs{:.2}",
+                    "Current Expense on Beauty and Personal Care: Rs{:.2}",
                     *bapc_rs
                 );
             }
@@ -471,7 +466,7 @@ fn bapc(
                         let date = "Date will be added later";
 
                         bapc_list.push(format!(
-                            "You spent Rs{:.2} on Beauty and Personal Care (BAPC) on {}",
+                            "You spent Rs{:.2} on Beauty and Personal Care on {}",
                             amount, date
                         ));
 
@@ -526,7 +521,7 @@ fn add_expense(
         ║ [4] Shopping                                      ║
         ║ [5] Education                                     ║
         ║ [6] Entertainment                                 ║
-        ║ [7] Beauty and Personal Care (BAPC)               ║
+        ║ [7] Beauty and Personal Care                      ║
         ║ [8] Exit                                          ║
         ╚═══════════════════════════════════════════════════╝
         "#);
@@ -618,10 +613,10 @@ fn show_category(category_name: &str, expense_list: &Vec<String>) {
             "#);
         }
 
-        "BEAUTY AND PERSONAL CARE (BAPC)" => {
+        "BAPC" => {
             println!(r#"
             ╔══════════════════════════════════════╗
-            ║                (BAPC)                ║
+            ║       BEAUTY AND PERSONAL CARE       ║
             ╚══════════════════════════════════════╝
             "#);
         }
@@ -640,39 +635,6 @@ fn show_category(category_name: &str, expense_list: &Vec<String>) {
     } else {
         for (index, expense) in expense_list.iter().enumerate() {
             println!("{}. {}", index + 1, expense);
-        }
-    }
-
-    loop {
-        println!(r#"
-        ╔══════════════════════════════╗
-        ║          SELECT OPTION       ║
-        ╠══════════════════════════════╣
-        ║ [1] View Expense             ║
-        ║ [2] Back                     ║
-        ╚══════════════════════════════╝
-        "#);
-
-        let input = read_input();
-
-        match input.as_str() {
-            "1" => {
-                if expense_list.is_empty() {
-                    println!("No expenses found.");
-                } else {
-                    for (index, expense) in expense_list.iter().enumerate() {
-                        println!("{}. {}", index + 1, expense);
-                    }
-                }
-            }
-
-            "2" => {
-                break;
-            }
-
-            _ => {
-                println!("Invalid Input");
-            }
         }
     }
 }
@@ -696,18 +658,18 @@ fn view_expense(
         "#);
 
         println!(r#"
-        ╔══════════════════════════════════════╗
-        ║       SELECT EXPENSE TYPE TO VIEW    ║
-        ╠══════════════════════════════════════╣
-        ║ [1] Food                             ║
-        ║ [2] Utility                          ║
-        ║ [3] Transport                        ║
-        ║ [4] Shopping                         ║
-        ║ [5] Education                        ║
-        ║ [6] Entertainment                    ║
-        ║ [7] Beauty and Personal Care (BAPC)  ║
-        ║ [8] Exit                             ║
-        ╚══════════════════════════════════════╝
+        ╔═══════════════════════════════════════╗
+        ║      SELECT EXPENSE TYPE TO VIEW      ║
+        ╠═══════════════════════════════════════╣
+        ║ [1] Food                              ║
+        ║ [2] Utility                           ║
+        ║ [3] Transport                         ║
+        ║ [4] Shopping                          ║
+        ║ [5] Education                         ║
+        ║ [6] Entertainment                     ║
+        ║ [7] Beauty and Personal Care          ║
+        ║ [8] Exit                              ║
+        ╚═══════════════════════════════════════╝
         "#);
 
         println!(r#"
@@ -744,7 +706,7 @@ fn view_expense(
             }
 
             "7" => {
-                show_category("(BAPC)", bapc_list);
+                show_category("BAPC", bapc_list);
             }
 
             "8" => {
@@ -798,10 +760,195 @@ fn total_spending(
 }
 
 
+fn list_edit(category_name: &str,expense_list: &mut Vec<String>,expense_rs: &mut f64) {
+    
+    match category_name {
+        "FOOD" => {
+            println!(r#"
+            ╔══════════════════════════════╗
+            ║        FOOD EXPENSES         ║
+            ╚══════════════════════════════╝
+            "#);
+        }
+
+        "UTILITY" => {
+            println!(r#"
+            ╔══════════════════════════════╗
+            ║       UTILITY EXPENSES       ║
+            ╚══════════════════════════════╝
+            "#);
+        }
+
+        "TRANSPORT" => {
+            println!(r#"
+            ╔══════════════════════════════╗
+            ║      TRANSPORT EXPENSES      ║
+            ╚══════════════════════════════╝
+            "#);
+        }
+
+        "SHOPPING" => {
+            println!(r#"
+            ╔══════════════════════════════╗
+            ║      SHOPPING EXPENSES       ║
+            ╚══════════════════════════════╝
+            "#);
+        }
+
+        "EDUCATION" => {
+            println!(r#"
+            ╔══════════════════════════════╗
+            ║      EDUCATION EXPENSES      ║
+            ╚══════════════════════════════╝
+            "#);
+        }
+
+        "ENTERTAINMENT" => {
+            println!(r#"
+            ╔══════════════════════════════╗
+            ║    ENTERTAINMENT EXPENSES    ║
+            ╚══════════════════════════════╝
+            "#);
+        }
+
+        "BAPC" => {
+            println!(r#"
+            ╔══════════════════════════════════════╗
+            ║       BEAUTY AND PERSONAL CARE       ║
+            ╚══════════════════════════════════════╝
+            "#);
+        }
+
+        _ => {
+            println!(r#"
+            ╔══════════════════════════════╗
+            ║           EXPENSES           ║
+            ╚══════════════════════════════╝
+            "#);
+        }
+    }
+
+    if expense_list.is_empty() {
+        println!("No Expenses added yet")
+    } else {
+            println!("Select Expense Index to Delete");
+        for (index, expense) in expense_list.iter().enumerate() {
+            println!("{}. {}", index + 1, expense);
+        }
+        
+        let length = expense_list.len();
+        let x= read_input();
+        let x: usize = x.trim().parse().unwrap();
+        
+        if x == 0 || x > length {
+            println!("Invalid Index");
+        } 
+            else {
+            let expense = &expense_list[x-1];
+            let part = expense.split(" ").nth(2).unwrap();
+            let part = part.trim_start_matches("Rs");
+            let part: f64 = part.trim().parse().unwrap();
+            println!("{}", part);
+            *expense_rs -= part;
+            expense_list.remove(x - 1);
+            println!("Expense removed successfully");
+        }
+    }
+}
+
+
+fn edit_expense(
+    food_list: &mut Vec<String>,
+    food_rs: &mut f64,
+    utility_list: &mut Vec<String>,
+    utility_rs: &mut f64,
+    transport_list: &mut Vec<String>,
+    transport_rs: &mut f64,
+    shopping_list: &mut Vec<String>,
+    shopping_rs: &mut f64,
+    education_list: &mut Vec<String>,
+    education_rs: &mut f64,
+    entertainment_list: &mut Vec<String>,
+    entertainment_rs: &mut f64,
+    bapc_list: &mut Vec<String>,
+    bapc_rs: &mut f64,
+) {
+    
+    loop {
+        println!(r#"
+        ╔══════════════════════════════════════════════════════════════╗
+        ║                         EDIT EXPENSES                        ║
+        ╚══════════════════════════════════════════════════════════════╝
+        "#);
+
+        println!(r#"
+        ╔═══════════════════════════════════════╗
+        ║      SELECT EXPENSE TYPE TO EDIT      ║
+        ╠═══════════════════════════════════════╣
+        ║ [1] Food                              ║
+        ║ [2] Utility                           ║
+        ║ [3] Transport                         ║
+        ║ [4] Shopping                          ║
+        ║ [5] Education                         ║
+        ║ [6] Entertainment                     ║
+        ║ [7] Beauty and Personal Care          ║
+        ║ [8] Exit                              ║
+        ╚═══════════════════════════════════════╝
+        "#);
+
+        println!(r#"
+        ╔══════════════════════════════╗
+        ║        SELECT OPTION         ║
+        ╚══════════════════════════════╝
+        "#);
+
+        let input = read_input();
+
+        match input.as_str() {
+            "1" => {
+                list_edit("FOOD", food_list, food_rs);
+            }
+
+            "2" => {
+                list_edit("UTILITY", utility_list, utility_rs);
+            }
+
+            "3" => {
+                list_edit("TRANSPORT", transport_list, transport_rs);
+            }
+
+            "4" => {
+                list_edit("SHOPPING", shopping_list, shopping_rs);
+            }
+
+            "5" => {
+                list_edit("EDUCATION", education_list, education_rs);
+            }
+
+            "6" => {
+                list_edit("ENTERTAINMENT", entertainment_list, entertainment_rs);
+            }
+
+            "7" => {
+                list_edit("BAPC", bapc_list, bapc_rs);
+            }
+
+            "8" => {
+                println!("Exiting...");
+                break;
+            }
+
+            _ => {
+                println!("Invalid Input");
+            }
+        }
+    }
+}
+
+
 fn search_expense() {
     println!("Search Expense feature will be added next.");
 }
-
 
 fn main() {
 
@@ -835,7 +982,8 @@ fn main() {
         ║   [2]  View Expenses                                                                 ║
         ║   [3]  Total Spending                                                                ║
         ║   [4]  Search Expenses                                                               ║
-        ║   [5]  Exit                                                                          ║
+        ║   [5]  Edit Expense                                                                  ║
+        ║   [6]  Exit                                                                          ║
         ║                                                                                      ║
         ╚══════════════════════════════════════════════════════════════════════════════════════╝
         "#);
@@ -897,7 +1045,26 @@ fn main() {
             }
 
             "5" => {
-                println!("Thank you for using Personal Expense Tracker!");
+                edit_expense(
+                    &mut food_list,
+                    &mut food_rs,
+                    &mut utility_list,
+                    &mut utility_rs,
+                    &mut transport_list,
+                    &mut transport_rs,
+                    &mut shopping_list,
+                    &mut shopping_rs,
+                    &mut education_list,
+                    &mut education_rs,
+                    &mut entertainment_list,
+                    &mut entertainment_rs,
+                    &mut bapc_list,
+                    &mut bapc_rs,
+                );
+            }
+
+            "6" => {
+                println!("Thank you for using our Personal Expense Tracker!");
                 break;
             }
 
